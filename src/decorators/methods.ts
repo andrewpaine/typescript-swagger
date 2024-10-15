cb1090257d3165b592fa87fd7fa134f74b071cb6
Andrew Paine <adnpaine@yahoo.co.uk>
2024-10-15 14:29:34 +0100

    Add decorators from typescript-rest

---
+++ src/decorators/methods.ts
@@ -0,0 +1,35 @@
+/** Document method as GET endpoint */
+export function GET(target: any, propertyKey: string): any {
+    return () => { return; };
+  }
+  
+  /** Document method as POST endpoint */
+  export function POST(target: any, propertyKey: string): any {
+    return () => { return; };
+  }
+  
+  /** Document method as PUT endpoint */
+  export function PUT(target: any, propertyKey: string): any {
+    return () => { return; };
+  }
+  
+  /** Document method as DELETE endpoint */
+  export function DELETE(target: any, propertyKey: string): any {
+    return () => { return; };
+  }
+  
+  /** Document method as HEAD endpoint */
+  export function HEAD(target: any, propertyKey: string): any {
+    return () => { return; };
+  }
+  
+  /** Document method as OPTIONS endpoint */
+  export function OPTIONS(target: any, propertyKey: string): any {
+    return () => { return; };
+  }
+  
+  /** Document method as PATCH endpoint */
+  export function PATCH(target: any, propertyKey: string): any {
+    return () => { return; };
+  }
+  
